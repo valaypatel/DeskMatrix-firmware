c875ce060cad3895588360c272101d47a1179734
MasterYoda <valaypatel85@gmail.com>
2026-09-06 13:14:13 +0530

    remove clock/weather Home dashboard (superseded by screensaver + Spotify)

--- firmware/DeskMatrix/screens/ClockWidget.h
+++
@@ -1,6 +0,0 @@
-// firmware/DeskMatrix/screens/ClockWidget.h
-#pragma once
-#include "../WidgetRegistry.h"
-#include "../services/RemoteClockService.h"
-
-void registerClockWidget(WidgetRegistry& registry, RemoteClockService& remoteClockService);
--- firmware/DeskMatrix/screens/WeatherWidget.h
+++
@@ -1,6 +0,0 @@
-// firmware/DeskMatrix/screens/WeatherWidget.h
-#pragma once
-#include "../WidgetRegistry.h"
-#include "../services/WeatherService.h"
-
-void registerWeatherWidget(WidgetRegistry& registry, WeatherService& weatherService);
